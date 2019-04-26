1aa793d285e20dbae2137569078642df85ef23ae
Lynn Liu <linjieliu7@gmail.com>
2019-04-26 10:15:56 +0800

    first commit

---
+++ SunnyDay/SunnyDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunnyDay
+//
+//  Created by admin on 2019/4/26.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
